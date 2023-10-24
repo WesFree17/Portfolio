9fc382ee1f868fa9e2da85089936482695219d34
Wes <Wes.Freeman17@gmail.com>
2023-10-24 16:23:22 -0500

    img wont turn for carousel

--- js/index.js
+++ js/index.js
@@ -7,5 +7,10 @@ buttons.forEach(button => {
 
         const activeSlide = slides.querySelector("[data-active]")
         let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1 
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeslide.dataset.active
     })      
 });
